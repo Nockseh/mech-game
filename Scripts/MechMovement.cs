caab1bdede30c607bf0901904a5280d11b706f3e
Nockseh <nockseh@gmail.com>
2024-12-21 01:22:26 -0500

    atan2 problem

--- Scripts/MechMovement.cs
+++ Scripts/MechMovement.cs
@@ -7,7 +7,11 @@ public partial class MechMovement : CharacterBody2D
 	public const float Move_Speed = 300.0f;
 	public const float Movement_Lerp = 0.25f;
 
-	public override void _PhysicsProcess(double delta) {
+    public override void _Ready() {
+		
+    }
+
+    public override void _PhysicsProcess(double delta) {
 		//Chassis position
 		Vector2 direction = Input.GetVector("Move_Left", "Move_Right", "Move_Up", "Move_Down");
 		Velocity = Velocity.Lerp(direction * Move_Speed, Movement_Lerp);
@@ -16,6 +20,8 @@ public partial class MechMovement : CharacterBody2D
 		//Hull orientation
 		Viewport viewport = GetViewport();
 		Vector2 mousePos = viewport.GetMousePosition();
-	}
-
+		Vector2 resolution = viewport.GetVisibleRect().Size;
+		mousePos += resolution/2;
+		GetNode<Sprite2D>("HullSprite").Rotation = Convert.ToSingle(Math.Atan2(mousePos.Y, mousePos.X));
+    }
 }
